3cf1ff897c3579dc8d5a2bd9b08c710adfaf5a7f
AMRITESH <amritesh67bakshi12@gmail.com>
2023-05-27 13:39:39 +0530

    more written in fraction.py file

--- fraction.py
+++ fraction.py
@@ -1,8 +1,24 @@
 class Fraction:
       def __init__(self,n,d):
             self.num=n
-            self.den=m
-            self.menu(n,d)
-      def menu(self,n,d)
+            self.den=d
+            #self.menu(n,d)
+      def __str__(self):
+            return f"{self.num}/{self.den}"
+      def __add__(self,other):
+            temp_num=self.num*other.den + other.num*self.den
+            temp_den=self.den*other.den
+            return f"{temp_num}/{temp_den}"
+      def __sub__(self,other):
+            temp_num=self.num*other.den - other.num*self.den
+            temp_den=self.den*other.den
+            return f"{temp_num}/{temp_den}"
+      def __mul__(self,other):
+            temp_num=self.num*other.num
+            temp_den=self.den*other.den
+            return f"{temp_num}/{temp_den}"
+            
+                  
+            
       
             
